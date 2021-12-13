0162bd336513ddc7e4286d12cbe2b779d72c26d7
Muhammad Abubakar <hcyma5@nottingham.edu.my>
2021-12-13 21:09:22 +0800

    made game graphic launcher

---
+++ src/Game/GraphicMain.java
@@ -0,0 +1,29 @@
+package Game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GraphicMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/view.fxml"));
+        primaryStage.setTitle("Brick Destroyer");
+        Scene scene=new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("brick.png"));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
